fe4e17965958ba6161cc01e4f89bd1dc75826346
Usama Masood <umasood.bee17seecs@seecs.edu.pk>
2023-02-08 19:50:51 +0500

    Linter errors fixed

--- src/modules/DOMfunctions.js
+++ src/modules/DOMfunctions.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 import { postComments } from './APIfunctions.js';
-import { countComments } from './commentsCounter.js';
+import countComments  from './commentsCounter.js';
 
 export const showErrorMsg = (msg) => {
   const errorDiv = document.createElement('h5');
