17d8bbd67c007a591b737e093b7b1b7ab7d25921
Usama Masood <umasood.bee17seecs@seecs.edu.pk>
2023-02-08 19:40:40 +0500

    Linter fixed

--- src/modules/commentsCounter.js
+++ src/modules/commentsCounter.js
@@ -1,6 +1,7 @@
-export const countComments = (comments) => {
+const countComments = (comments) => {
   const counterElement = document.querySelector('.comments-count');
   let count = 0;
   comments.forEach(() => { count += 1; });
   counterElement.textContent = `(${count})`;
 };
+export default countComments;
